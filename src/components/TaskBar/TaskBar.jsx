b9318303658ca684e74f80ead2de5923d67ac07e
k-gourav <code2change.gourav@gmail.com>
2024-10-26 23:32:34 +0530

    feat: add main TaskBar Component

---
+++ src/components/TaskBar/TaskBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './TaskBar.module.css';
+
+const TaskBar = () => {
+  return (
+    <aside>
+        <section className={styles.taskbar__container}>
+            
+          </section>
+
+    </aside>
+  )
+}
+
+export default TaskBar;
